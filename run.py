e11ef7fabcdd0fe446a9265088555d0bd2646909
MaximilianKing <Maximilian_King@brown.edu>
2020-08-12 23:24:13 -0400

    Tightened prints

--- run.py
+++ run.py
@@ -9,24 +9,23 @@ env = gym.make("raaspendulum-v0")
 env.reset()
 obs = []
 
-print("\nBeginning ping test to google...")
+print("\nBeginning ping test to google.com ...")
 print(system("ping -c 1 www.google.com"))
 
-print("\nBeginning download with google html response wtih urlopen")
+print("\nBeginning google.com html download with urlopen ...")
 try:
     response = urllib.request.urlopen("http://www.google.com/")
     html = response.read()
     print(html)
 except:
-    print("Download has failed using `urlopen`. Moving on.")
+    print("Download has failed using `urlopen`.")
 
-
-print("\nBeginning file download with urlretrieve")
+print("\nBeginning file download with urlretrieve ...")
 try:
     url = "http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg"
     cat = urllib.request.urlretrieve(url)
     print(cat[1])
 except:
-    print("Download has failed using `urlretrieve`. Moving on.")
+    print("Download has failed using `urlretrieve`.")
 
 print("Job done.")
