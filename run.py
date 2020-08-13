f1b0dc62eb50968bbcc299b1af5b92008d149945
MaximilianKing <Maximilian_King@brown.edu>
2020-08-12 23:08:22 -0400

    Attempting urlopen

--- run.py
+++ run.py
@@ -3,18 +3,21 @@ import gym_raas
 import numpy as np
 import time
 
-
 env = gym.make('raaspendulum-v0')
 env.reset()
 obs = []
 
 from os import system
-system("ping -c 5 www.google.com")
+system("ping -c 1 www.google.com")
 
 import urllib.request
 
-print('Beginning file download with urllib2...')
+print('\nBeginning download with google html response wtih urlopen...')
+response = urllib.request.urlopen('http://www.google.com/')
+html = response.read()
+print(html)
 
+print('\nBeginning file download with urlretrieve...')
 url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
 cat = urllib.request.urlretrieve(url)
 print(cat[1])
