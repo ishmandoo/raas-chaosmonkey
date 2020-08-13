ec5832a10375357fae2754ef13218f62a091ef9e
MaximilianKing <Maximilian_King@brown.edu>
2020-08-12 22:57:24 -0400

    Testing just in memory download

--- run.py
+++ run.py
@@ -12,5 +12,5 @@ import urllib.request
 print('Beginning file download with urllib2...')
 
 url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
-cat = urllib.request.urlretrieve(url, 'cat.jpg')
+cat = urllib.request.urlretrieve(url)
 print(cat[1])
