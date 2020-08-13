4eae2bd61c9a34c6dbf17b69aa4b6b0477b6733f
MaximilianKing <Maximilian_King@brown.edu>
2020-08-12 23:14:53 -0400

    Try excepting each test

--- run.py
+++ run.py
@@ -2,22 +2,34 @@ import gym
 import gym_raas
 import numpy as np
 import time
+from os import system
+import urllib.request
 
-env = gym.make('raaspendulum-v0')
+env = gym.make("raaspendulum-v0")
 env.reset()
 obs = []
 
-from os import system
-system("ping -c 1 www.google.com")
+print("Beginning ping test to google...")
+try:
+    system("ping -c 1 www.google.com")
+except:
+    print("Ping test has failed. Moving on.")
 
-import urllib.request
+print("Beginning download with google html response wtih urlopen")
+try:
+    response = urllib.request.urlopen("http://www.google.com/")
+    html = response.read()
+    print(html)
+except:
+    print("Download has failed using `urlopen`. Moving on.")
 
-print('\nBeginning download with google html response wtih urlopen...')
-response = urllib.request.urlopen('http://www.google.com/')
-html = response.read()
-print(html)
 
-print('\nBeginning file download with urlretrieve...')
-url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
-cat = urllib.request.urlretrieve(url)
-print(cat[1])
+print("Beginning file download with urlretrieve")
+try:
+    url = "http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg"
+    cat = urllib.request.urlretrieve(url)
+    print(cat[1])
+except:
+    print("Download has failed using `urlretrieve`. Moving on.")
+
+print("Job done.")
